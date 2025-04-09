2597271e5f41cbe40918c369f5cd01766bf091d5
olesia73 <ole.bilyk@gmail.com>
2025-04-09 18:41:31 -0400

    work in progress

--- Week11/HW/script.js
+++ Week11/HW/script.js
@@ -1,1 +1,21 @@
 let list = ["Kyiv cake", "clouds", "Peter Pan", "princess", "cubes", "Game of Thrones", "flowers", "chess"];
+// const button = document.getElementById(list[0]);
+
+// function randomOne() {
+//     alert("fdhjsk");
+// }
+// button.addEventListener("click", function() { randomOne() });
+
+function printBtn() {
+    // for (var i = 0; i < listBrand.length; i++) {
+    //     var btn = document.createElement("button");
+    //     var t = document.createTextNode(listBrand[i]);
+    //     btn.appendChild(t);
+    //     document.body.appendChild(btn);
+    //  }
+     for (var i = 0; i < list.length; i++) {
+        const button = document.createElement("button");
+        button.value = list[i];
+        document.body.appendChild(button);
+     }
+}
