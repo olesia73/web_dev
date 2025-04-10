c059f77e8e809a3ea0ced59ce45965dde9cf1848
olesia73 <ole.bilyk@gmail.com>
2025-04-10 00:11:36 -0400

    cakes

--- Week11/HW/script.js
+++ Week11/HW/script.js
@@ -1,21 +1,21 @@
 let list = ["Kyiv cake", "clouds", "Peter Pan", "princess", "cubes", "Game of Thrones", "flowers", "chess"];
-// const button = document.getElementById(list[0]);
+let imageOrNot = [0, 0, 0, 0, 0, 0, 0, 0];
 
-// function randomOne() {
-//     alert("fdhjsk");
-// }
-// button.addEventListener("click", function() { randomOne() });
+function openImage(index) {
+   // console.log(index);
+   if (imageOrNot[index]) {
+      document.getElementById(String(index)).innerHTML = list[index];
+      imageOrNot[index] = !imageOrNot[index];
+   }
+   else {
+      document.getElementById(String(index)).innerHTML = `<img src="${list[index]}.jpeg" alt="Description of the image" />`;
+      imageOrNot[index] = !imageOrNot[index];
+   }
+}
 
-function printBtn() {
-    // for (var i = 0; i < listBrand.length; i++) {
-    //     var btn = document.createElement("button");
-    //     var t = document.createTextNode(listBrand[i]);
-    //     btn.appendChild(t);
-    //     document.body.appendChild(btn);
-    //  }
-     for (var i = 0; i < list.length; i++) {
-        const button = document.createElement("button");
-        button.value = list[i];
-        document.body.appendChild(button);
-     }
+for (var i = 0; i < list.length; i++) {
+   document.getElementById(String(i)).innerHTML = list[i];
+   console.log(i);
+   const kar = i;
+   document.getElementById(String(i)).addEventListener("click", function() { openImage(kar) });
 }
